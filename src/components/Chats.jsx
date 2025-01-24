391c7d442ff9d9195d596b3a2780a383685216c3
Savita Rao <savitarao702@gmail.com>
2025-01-24 14:15:00 +0530

    pushing barabari project

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,33 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/Chats.css";
 
 export const Chats = () => {
+  const [showChatPage, setShowChatPage] = useState(false);
+
+  // Function to handle button click
+  const handleChatClick = () => {
+    setShowChatPage(true);
+  };
+
+  // Function to go back to the main page
+  const handleBackClick = () => {
+    setShowChatPage(false);
+  };
+
   return (
     <div>
-      <div className="logoDiv">
-        <img src="logo.png" alt="Barabari logo" />
-        <p>The Barabari Project</p>
-      </div>
-      <div className="publicImgDiv">
-      <div className="logoDivPara">
-        <h1>
-          Let our students <br /> build your <span>ChatBots</span>
-        </h1>
-        <p>
-          We are Section 8 Non-Profit upskilling candidates <br />
-          from low-income families democratizing access <br />
-          to tech design employment opportunities
-        </p>
-        {/* Button as an anchor link */}
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfcfOaRlK0prJLPZImUX5-cQllWitpmv4Ypp6kjCoAcEjJGFA/viewform" target="_blank" rel="noopener noreferrer">
-          <button>Hire from us</button>
-        </a>
-      </div>
-      <div>
-        <img className="publicImg" src="public.png" alt="A public Image" />
-        <button className="chatButton">Chat with us</button>
-      </div>
-    </div>
+      {!showChatPage ? (
+        <div>
+          <div className="logoDiv">
+            <img src="logo.png" alt="Barabari logo" />
+            <p>The Barabari Project</p>
+          </div>
+          <div className="publicImgDiv">
+            <div className="logoDivPara">
+              <h1>
+                Let our students <br /> build your <span>ChatBots</span>
+              </h1>
+              <p>
+                We are Section 8 Non-Profit upskilling candidates <br />
+                from low-income families democratizing access <br />
+                to tech design employment opportunities
+              </p>
+              <button>Hire from us</button>
+            </div>
+            <div>
+              <img className="publicImg" src="public.png" alt="A public Image" />
+              <button className="chatButton" onClick={handleChatClick}>
+                Chat with us
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="chatPage">
+          <button className="backButton" onClick={handleBackClick}>
+            Back
+          </button>
+          <div className="chatContainer">
+            <div className="backChat">
+            <div onClick={handleBackClick}>|-</div>
+            <div> Chat with us  </div>
+            <div onClick={handleBackClick}>x</div>
+            </div>
+            <img src="search.png" alt="" />
+            <h1>Hi There!</h1>
+            <p>Want to work with us?</p>
+            <p>
+              We are happy to discuss any potential projects. If you have a
+              couple of minutes, we would love to hear more.
+            </p>
+            <button className="startChatButton">Start Chat</button>
+            <p>Got any other queries?</p>
+            <p>Please drop us an email at barabaricollective@gmail.com</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
